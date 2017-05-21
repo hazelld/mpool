ecf8b6eee56e8d9aba39c75c818645565484af17
William Hazell <whazell@mail.uoguelph.ca>
2017-05-20 20:07:54 -0400

    Finished rough skeleton

--- mpool.c
+++ mpool.c
@@ -1,24 +1,94 @@
 #include "mpool.h"
 
 
-/*
- *	This is the main structure used within this library. It defines a block of 
- *	memory that is within the pool. The address it points too will always be 
- *	a valid block of allocated memory.
- *
- *	This is a doubley linked list to allow for fast insertions and deletions, as
- *	that is all we care about. The list will never be traversed, only added too 
- *	and deleted from. When the user allocates memory from this pool, the block
- *	is removed from the front. When the user deallocates memory it is readded 
- *	to the back of the list. This also applies when the user requests more 
- *	memory.
- *
- */
 struct _block {
 	struct _block* next;
-	struct _block* prev;
 	void* addr;
 };
+
+/**/
+struct mpool {
+	struct _block* block_list;
+	struct _block* unused_blocks;
+	size_t block_size;
+	
+	int32_t capacity;
+	int32_t block_list_size;
+	int32_t unused_block_list_size;
+	
+	void** blobs;
+	size_t* blob_sizes;
+	int alloc_count;
+	
+#ifdef MULTITHREAD
+	LOCK_TYPE block_list_mutex;
+	LOCK_TYPE unused_block_list_mutex;
+#endif
+};
+
+static mpool_error _remove_block_list (struct _block** block, struct _block** list) 
+{
+	if (block == NULL || list == NULL)
+		return MPOOL_ERR_NULL_ARG;
+	if (*list == NULL)
+		return MPOOL_EMPTY_LIST;
+
+	*block = *list;
+	*list = (*block)->next;
+	return MPOOL_SUCCESS; 
+}
+
+static mpool_error _insert_block_list ( struct _block* block, struct _block** list)
+{
+	if (block == NULL || list == NULL)
+		return MPOOL_ERR_NULL_ARG;
+
+	block->next = *list;
+	*list = block;
+	return MPOOL_SUCCESS;
+}
+
+
+static mpool_error _add_to_unused_list(struct _block* new_block, struct mpool* pool) 
+{
+
+#ifdef MULTITHREAD
+	if (MUTEX_LOCK(&pool->unused_block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
+	
+	mpool_error err = _insert_block_list(new_block, &pool->unused_blocks);
+
+#ifdef MULTITHREAD
+	if (MUTEX_UNLOCK(&pool->unused_block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
+
+	if (err == MPOOL_SUCCESS)
+		pool->unused_block_list_size++;
+
+	return MPOOL_SUCCESS;
+}
+
+static mpool_error _remove_from_unused_list(struct _block** block, struct mpool* pool)
+{
+
+#ifdef MULTITHREAD
+	if (MUTEX_LOCK(&pool->unused_block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
+
+	mpool_error err = _remove_block_list(block, &pool->unused_blocks);
+
+#ifdef MULTITHREAD
+	if (MUTEX_UNLOCK(&pool->unused_block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
+	
+	if (err == MPOOL_SUCCESS)
+		pool->unused_block_list_size--;
+	return MPOOL_SUCCESS;
+}
 
 
 static mpool_error _create_block (void* addr, struct _block** block) 
@@ -32,63 +102,55 @@ static mpool_error _create_block (void* addr, struct _block** block)
 	}
 
 	(*block)->next = NULL;
-	(*block)->prev = NULL;
 	(*block)->addr = addr;
 	return MPOOL_SUCCESS;
 }
 
 static mpool_error _add_block (struct _block* new_block, struct mpool* pool) 
 {
-
-#ifdef PTHREAD
-	if (pthread_mutex_lock(&pool->insert_mutex) != 0)
-		return MPOOL_ERR_MUTEX;
-#endif
-
-	if (pool->list_size + 1 > pool->capacity)
+	if (pool->block_list_size + 1 > pool->capacity)
 		return MPOOL_FULL_LIST;
 
-	struct _block* buff = pool->block_list_end;
-
-	/* If the list is empty, make sure we have ptr to start and end */
-	if (buff == NULL) {
-		pool->block_list_end = new_block;
-		pool->block_list_start = new_block;
-	} else {
-		buff->next = new_block;
-		new_block->prev = buff;
-		pool->block_list_end = new_block;
-	}
+#ifdef MULTITHREAD
+	if (MUTEX_LOCK(&pool->block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
 	
-	pool->list_size++;
-#ifdef PTHREAD
-	if (pthread_mutex_unlock(&pool->insert_mutex) != 0)
+	mpool_error err = _insert_block_list(new_block, &pool->block_list);
+
+#ifdef MULTITHREAD
+	if (MUTEX_UNLOCK(&pool->block_list_mutex) != 0)
 		return MPOOL_ERR_MUTEX;
 #endif
 
-	return MPOOL_SUCCESS;
+	if (err == MPOOL_SUCCESS)
+		pool->block_list_size++;
+	return err;
 }
 
 static mpool_error _remove_block (struct _block** block, struct mpool* pool) 
 {
+	if (block == NULL || pool == NULL)
+		return MPOOL_ERR_NULL_ARG;
 
-#ifdef PTHREAD
-	if (pthread_mutex_lock(&pool->remove_mutex) != 0)
-		return MPOOL_ERR_MUTEX;
-#endif
-
-	if (pool->block_list_start == NULL)
+	if (pool->block_list == NULL)
 		return MPOOL_EMPTY_LIST;
 
-	*block = pool->block_list_start;
-	pool->block_list_start = (*block)->next;
-	pool->block_list_start->prev = NULL;
-	pool->list_size--;
-
-#ifdef PTHREAD
-	if (pthread_mutex_unlock(&pool->remove_mutex) != 0)
+#ifdef MULTITHREAD
+	if (MUTEX_LOCK(&pool->block_list_mutex) != 0)
 		return MPOOL_ERR_MUTEX;
 #endif
+
+	mpool_error err = _remove_block_list(block, &pool->block_list);
+
+#ifdef MULTITHREAD
+	if (MUTEX_UNLOCK(&pool->block_list_mutex) != 0)
+		return MPOOL_ERR_MUTEX;
+#endif
+	
+	if (err == MPOOL_SUCCESS)
+		pool->block_list_size--;
+	return err;
 }
 
 mpool_error _partition_blob (struct mpool* pool, int index)
@@ -116,17 +178,17 @@ mpool_error init_mpool (size_t block_size, int32_t capacity, struct mpool** pool
 	if (pool == NULL)
 		return MPOOL_ERR_NULL_ARG;
 
-	*pool = calloc(sizeof(struct mpool));
+	*pool = calloc(1, sizeof(struct mpool));
 	if (*pool == NULL)
 		return MPOOL_ERR_ALLOC;
 	
 	(*pool)->block_size = block_size;
 	(*pool)->capacity = capacity;
 	
-#ifdef PTHREAD
-	if (pthread_mutex_init(&(*pool)->insert_mutex) != 0)
+#ifdef MULTITHREAD
+	if (MUTEX_INIT(&(*pool)->block_list_mutex, NULL) != 0)
 			return MPOOL_ERR_MUTEX;
-	if (pthread_mutex_init(&(*pool)->remove_mutex) != 0)
+	if (MUTEX_INIT(&(*pool)->unused_block_list_mutex, NULL) != 0)
 		return MPOOL_ERR_MUTEX;
 #endif
 	
@@ -139,4 +201,92 @@ mpool_error init_mpool (size_t block_size, int32_t capacity, struct mpool** pool
 	(*pool)->blob_sizes = malloc(sizeof(size_t));
 	(*pool)->blob_sizes[0] = block_size * capacity;
 	mpool_error err = _partition_blob(*pool, (*pool)->alloc_count);
+	return MPOOL_SUCCESS;
+}
+
+
+mpool_error mpool_alloc (void** item, struct mpool* pool) 
+{
+	struct _block* b;
+	mpool_error err = MPOOL_SUCCESS;
+	
+	if (item == NULL || pool == NULL) 
+		return MPOOL_ERR_NULL_ARG;
+
+	if ((err = _remove_block(&b, pool)) != MPOOL_SUCCESS)
+		return err;
+	
+	*item = b->addr;
+	err = _add_to_unused_list(b, pool);
+	return err;
+}
+
+
+mpool_error mpool_dealloc (void* item, struct mpool* pool) 
+{
+	struct _block* b;
+	mpool_error err = MPOOL_SUCCESS;
+
+	if (item == NULL || pool == NULL) 
+		return MPOOL_ERR_NULL_ARG;
+	
+	if ((err = _remove_from_unused_list(&b, pool)) != MPOOL_SUCCESS) 
+		return err;
+	
+	b->addr = item;
+	err = _add_block(b, pool);
+	return err;
+}
+
+
+mpool_error mpool_realloc (int32_t new_capacity, struct mpool* pool) 
+{
+	mpool_error err = MPOOL_SUCCESS;
+
+	if (pool == NULL) 
+		return MPOOL_ERR_NULL_ARG;
+	if (pool->capacity >= new_capacity)
+		return MPOOL_ERR_INVALID_REALLOC_SIZE;
+	
+	int32_t extra = new_capacity - pool->capacity;
+	size_t new_size = extra * pool->block_size;
+	
+	pool->blobs = realloc(pool->blobs, sizeof(void*) * ++pool->alloc_count);
+	if (pool->blobs == NULL) return MPOOL_ERR_ALLOC;
+
+	pool->blobs[pool->alloc_count - 1] = malloc(new_size);
+	if (pool->blobs[pool->alloc_count - 1] == NULL) return MPOOL_ERR_ALLOC;
+
+	pool->blob_sizes = realloc(pool->blob_sizes, sizeof(size_t) * pool->alloc_count);
+	if (pool->blob_sizes == NULL) return MPOOL_ERR_ALLOC;
+
+	pool->blob_sizes[pool->alloc_count] = new_size;
+	err = _partition_blob(pool, pool->alloc_count);
+	return err;
+}
+
+
+mpool_error free_mpool (struct mpool* pool)
+{
+	struct _block* buff;
+	struct _block* b = pool->block_list;
+	
+	while (b) {
+		buff = b->next;
+		free(b);
+		b = buff;
+	}
+	
+	b = pool->unused_blocks;
+	while (b) {
+		buff = b->next;
+		free(b);
+		b = buff;
+	}
+
+	for (int i = 0; i < pool->alloc_count; i++) 
+		free(pool->blobs[i]);
+	free(pool->blobs);
+	free(pool->blob_sizes);
+	free(pool);
 }
--- mpool.h
+++ mpool.h
@@ -8,12 +8,13 @@
 
 #ifdef _WIN32
 #	warn No multi-threaded support currently
-#elif __APPLE__
-#	define PTHREAD 1
+#elif __APPLE__ || __linux__
 #	include <pthread.h>
-#elif __linux__
-#	define PTHREAD 1
-#	include <pthread.h>
+#	define MULTITHREAD 1
+#	define LOCK_TYPE pthread_mutex_t
+#	define MUTEX_LOCK pthread_mutex_lock
+#	define MUTEX_UNLOCK pthread_mutex_unlock
+#	define MUTEX_INIT pthread_mutex_init
 #else
 #	warn Unknown compiler being used.
 #endif
@@ -25,38 +26,22 @@ typedef enum mpool_error {
 	MPOOL_ERR_INVALID_SIZE,
 	MPOOL_ERR_NULL_ARG,
 	MPOOL_ERR_MUTEX,
+	MPOOL_ERR_INVALID_REALLOC_SIZE,
 	MPOOL_FULL_LIST,
 	MPOOL_EMPTY_LIST,
 } mpool_error;
 
 
-struct _block;
-
-struct mpool {
-	struct _block* block_list_start;
-	struct _block* block_list_end;
-	size_t block_size;
-	int32_t capacity;
-	int32_t list_size;
-	void** blobs;
-	size_t* blob_sizes;
-	int alloc_count;
-	
-#ifdef PTHREAD
-	pthread_mutex_t remove_mutex;
-	pthread_mutex_t insert_mutex;
-#endif
-
-};
+struct mpool;
 
 
 mpool_error init_mpool (size_t block_size, int32_t capacity, struct mpool** pool);
 
-mpool_error mpool_alloc (void** item, struct mpool** pool);
+mpool_error mpool_alloc (void** item, struct mpool* pool);
 
-mpool_error mpool_dealloc (void* item, struct mpool** pool);
+mpool_error mpool_dealloc (void* item, struct mpool* pool);
 
-mpool_error mpool_realloc (int32_t new_capacity, struct mpool** pool);
+mpool_error mpool_realloc (int32_t new_capacity, struct mpool* pool);
 
 mpool_error free_mpool (struct mpool* pool);
 
