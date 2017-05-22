0a196a68866f8098cf942d4d19432f6bf6e50342
William Hazell <whazell@mail.uoguelph.ca>
2017-05-21 22:27:12 -0400

    Started documenting code

--- mpool.c
+++ mpool.c
@@ -1,20 +1,89 @@
+/*
+ * mpool.c --- Implementation of a fixed size memory pool 
+ *
+ * Copyright (c) 2017 William Hazell
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "mpool.h"
 
-
+/**
+ * struct _block - Linked list holding all the blocks of memory.
+ * 
+ * @next: Pointer to the next struct _block
+ * @addr: Address of the block it points too.
+ *
+ *
+ *	This structure's purpose is to hold the location of a single block within a
+ *	blob that has been allocated. When the user alloc's memory, a _block is 
+ *	removed from the chain of _blocks, and the address it holds is returned to 
+ *	the user. When the user dealloc's an address, it is put inside a currently
+ *	empty _block and is added back into the chain.
+ *
+ *	Note there is a 1:1 ratio between _blocks and the pool's capacity, and this
+ *	never changes. These _blocks are malloc'd when the pool is init, and only
+ *	freed when free_mpool() is called. Once the _block's memory address is 
+ *	given to the user, it is put into the unused_blocks list, until the user
+ *	deallocates the space.
+ */
 struct _block {
 	struct _block* next;
 	void* addr;
 };
 
-/**/
+
+/**
+ * struct mpool - Main data structure holding everything the pool needs.
+ * 
+ * @block_list: List of _blocks with free memory addresses
+ * @unused_blocks: List of currently unused _blocks
+ * @block_size_list: Size of the block list to check if list is full
+ * @unused_block_size_list: Size of the unused block list
+ * @block_size: Size of each individual block 
+ * @capacity: How many blocks the user needs 
+ * @blobs: This array holds the chunks of memory that are allocated
+ * @blob_sizes: Array of the sizes corresponding to blobs array
+ * @alloc_count: How many times memory has been alloc'd (size of blobs array)
+ * @block_list_mutex: Holds the lock to @block_list
+ * @unused_block_list_mutex: Holds the lock to @unused_block_list
+ *
+ *
+ * 	This structure holds all the needed information for the pool to function.
+ * 	When the user uses init_mpool() or mpool_realloc() and memory is needed 
+ * 	from the kernel, the "blobs" of memory are stored in the @blobs array. This
+ * 	is to keep track of all malloc'd memory so it may be free'd after. This 
+ * 	blob is then internally partitioned into _blocks that hold @capacity amount
+ * 	of @block_size chunks.
+ *
+ * 	The lists are locked by a mutex, for both insert and remove operations, and
+ * 	are only included if multithreading is enabled.
+ *
+ */
 struct mpool {
 	struct _block* block_list;
 	struct _block* unused_blocks;
-	size_t block_size;
-	
-	int32_t capacity;
 	int32_t block_list_size;
 	int32_t unused_block_list_size;
+	
+	size_t block_size;	
+	int32_t capacity;
 	
 	void** blobs;
 	size_t* blob_sizes;
@@ -172,6 +241,13 @@ mpool_error _partition_blob (struct mpool* pool, int index)
 	return MPOOL_SUCCESS;
 }
 
+
+/************************************************
+ *
+ *	Public Functions --- See mpool.h for function
+ *	headers.
+ *
+ ***********************************************/
 
 mpool_error init_mpool (size_t block_size, int32_t capacity, struct mpool** pool) 
 {
